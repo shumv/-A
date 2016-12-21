27536d26448e4eb14c69567419f9165fa8557c45
Vitaliy <shumkovitaliy@gmail.com>
2016-12-21 19:40:13 +0300

    Added BusinessLogic

---
+++ BusinessLogic/NewsManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    class NewsManager: INewsManager
+    {
+        public void AddNews()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
